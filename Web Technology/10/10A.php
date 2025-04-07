749b9ab3134a63d1f8bdc2eb62a78368c54b7917
Gaurav Mandal <37218716+GauravM512@users.noreply.github.com>
2025-04-07 20:35:56 +0530

    Update 10A.php

--- Web Technology/10/10A.php
+++ Web Technology/10/10A.php
@@ -1,12 +1,5 @@
-<?//Write a PHP script to display source code of a webpage.?>
 <?php
-    $url = "https://example.com";
-
-    $sourceCode = file_get_contents($url);
-
-    if ($sourceCode !== false) {
-        echo "<pre>" . htmlspecialchars($sourceCode) . "</pre>";
-    } else {
-        echo "Failed to retrieve source code from $url";
-    }
+$file = "sample.html";
+echo "<h3>Source Code of '$file':</h3>";
+echo "<pre>" . htmlspecialchars($sourceCode) . "</pre>";
 ?>
